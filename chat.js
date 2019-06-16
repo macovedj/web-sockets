197a1083e14beac409479b5650636cb66bb4ff2a
Daniel Macovei <djmacovei@gmail.com>
2019-06-16 15:18:08 -0400

    make messages not be overwritten

--- chat.js
+++ chat.js
@@ -9,7 +9,6 @@ var message = document.getElementById('message'),
     feedback = document.getElementById('feedback');
 
 //Emit events
-
 btn.addEventListener('click', function(){
   socket.emit('chat', {
     message: message.value,
@@ -20,9 +19,10 @@ btn.addEventListener('click', function(){
 message.addEventListener('keypress', function(){
   socket.emit('typing', handle.value);
 });
+
 //Listen for events
 socket.on('chat', function(data){
-  output.innerHTML = "";
+  feedback.innerHTML = "";
   output.innerHTML += '<p><strong>' + data.handle + ':</strong>' + data.message + '</p>';
 });
 
